3dc3bb3ef59ae639909eb6ef3e2e41c7854f02d3
Linuslan <linuslan@sina.cn>
2018-08-16 08:33:20 +0800

    111

--- SaleOA-SA/src/com/saleoa/ui/LevelPanel.java
+++ SaleOA-SA/src/com/saleoa/ui/LevelPanel.java
@@ -1,23 +1,27 @@
 package com.saleoa.ui;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 import java.util.Vector;
 
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JToolBar;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableColumn;
 
+import com.saleoa.common.utils.JdbcHelper;
 import com.saleoa.common.utils.ModuleName;
 import com.saleoa.model.Level;
 
 
 public class LevelPanel extends JPanel {
+	private static Dimension screenSize = MainEntry.getScreanSize();
 	public LevelPanel() {
 		this.setName(ModuleName.LEVEL);
 		init();
@@ -26,9 +30,10 @@ public class LevelPanel extends JPanel {
 	public void init() {
 		final Vector<Vector<String>> row = new Vector<Vector<String>> ();
 		final Vector<String> cols = new Vector<String>();
-		cols.add("column1");
-		cols.add("column2");
-		DefaultTableModel model = new DefaultTableModel();
+		cols.add("编号");
+		cols.add("等级");
+		cols.add("积分");
+		DefaultTableModel model = new DefaultTableModel(row, cols);
 		final JTable table = new JTable(model);
 		this.setLayout(new BorderLayout());
 		JToolBar toolBar = new JToolBar();
@@ -38,35 +43,36 @@ public class LevelPanel extends JPanel {
 		addBtn.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent arg0) {
-				
-				Vector<String> newRow = new Vector<String> ();
-				newRow.add("测试");
-				newRow.add("10");
-				row.add(newRow);
-				DefaultTableModel model = new DefaultTableModel(row, cols);
-				table.setModel(model);
+				JDialog dialog = new JDialog();
+				int dialogWidth = 300;
+				int dialogHeight = 300;
+				dialog.setSize(dialogWidth, dialogHeight);
+				dialog.setLocation((screenSize.width-dialogWidth)/2, (screenSize.height-dialogHeight)/2);
+				dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+				dialog.setVisible(true);
 			}
 			
 		});
 		toolBar.add(addBtn);
-		
-		String[] columnNames = {"名称","升级所需积分"};// 定义表格列名数组
-        // 定义表格数据数组
-        String[][] tableValues = {{"A1","B1"},{"A2","B2"},{"A3","B3"},{"A4","B4"},{"A5","B5"}};
         
-        
-        // 创建指定列名和数据的表格
-        
-        
-        /*Vector vRow1 = new Vector();
-        vRow1.add("cell 2 0");
-        vRow1.add("cell 2 1");
-        vData.add(vRow1);
-        model = new DefaultTableModel(vData, vName);
-        table.setModel(model);*/
         // 创建显示表格的滚动面板
         JScrollPane scrollPane = new JScrollPane(table);
         // 将滚动面板添加到边界布局的中间
         this.add(scrollPane, BorderLayout.CENTER);
+        try {
+        	List<Level> levels = (List<Level>) JdbcHelper.select("SELECT * FROM tbl_oa_level", Level.class);
+        	for(int i = 0; i < levels.size(); i ++) {
+        		Level level = levels.get(i);
+        		Vector<String> newRow = new Vector<String> ();
+				newRow.add(String.valueOf(level.getId()));
+				newRow.add(levels.get(i).getName());
+				newRow.add(String.valueOf(level.getRewardPoints()));
+				row.add(newRow);
+        	}
+        	model = new DefaultTableModel(row, cols);
+			table.setModel(model);
+        } catch(Exception ex) {
+        	ex.printStackTrace();
+        }
 	}
 }
