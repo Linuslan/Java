322bc6223fe29f26951dc7a05c8fbf12512d6ac5
Linuslan <linuslan@sina.cn>
2018-09-12 16:29:41 +0800

    111

--- SaleOA-SA/src/com/saleoa/common/utils/USBUtil.java
+++ SaleOA-SA/src/com/saleoa/common/utils/USBUtil.java
@@ -8,7 +8,7 @@ import java.io.InputStreamReader;
 
 public class USBUtil {
 	//
-	private static String[] snArr = {"b56f71490e14abcf42e5500a3344329d", "dc3d814bc10a67b1dc2357461d86aa0e"};
+	private static String[] snArr = {"b56f71490e14abcf42e5500a3344329d", "dc3d814bc10a67b1dc2357461d86aa0e", "f89a78304d57b5059a7599a140f84eee"};
 	public static String getSerialNumber(String drive) {
 		String result = "";
 		try {
@@ -110,6 +110,47 @@ public class USBUtil {
 		return check;
 	}
 	
+	public static void getSerialNo() {
+		String commond = "reg query HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\services\\USBSTOR\\Enum";
+		String vid = "";
+		String pid = "";
+		String sn = "";
+		int count = 0;
+		boolean check = false;
+		try {
+			// 获取注册表信息
+			Process ps = null;
+			ps = Runtime.getRuntime().exec(commond);
+			ps.getOutputStream().close();
+			InputStreamReader i = new InputStreamReader(ps.getInputStream());
+			String line;
+			BufferedReader ir = new BufferedReader(i);
+			// 将信息分离出来
+			while ((line = ir.readLine()) != null) {
+				if (line.contains("USB\\VID")) {
+					count++;
+					for (String s : line.split("    ")) {
+						System.out.println(s);
+						if (s.contains("USB\\VID")) {
+							for (String ss : s.split("\\\\")) {
+								if (ss.contains("VID")) {
+									String[] ssArr = ss.split("&");
+									vid = ssArr[0];
+									pid = ssArr[1];
+								}else {
+									sn = ss;
+									System.out.println(sn);
+								}
+							}
+						}
+					}
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) {
 		//String md5 = MD5Util.md5(MD5Util.md5("Zynywmml00"));
 		//System.out.println(md5);
@@ -119,6 +160,7 @@ public class USBUtil {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}*/
-		//System.out.println(MD5Util.md5(MD5Util.md5("60A44C3FAC7DE26109720AF8")));
+		getSerialNo();
+		System.out.println(MD5Util.md5(MD5Util.md5("0013729B6EB8BB319559025E")));
 	}
 }
